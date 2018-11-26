04a159585fbb69f1bc18299cb2db945c89495000
Joe Badaczewski <badaczewskij@live.com>
2018-11-26 12:40:07 -0500

    adding react-responsive

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 #### Tehnical Features:
 * [React Spring](https://github.com/drcmda/react-spring) for animation and parallax effect
+* [React Responsive](https://github.com/contra/react-responsive) as the framework for managing responsiveness (mobile-first design)
+* [Stylus](https://github.com/stylus/stylus) as a robust, expressive styling language that compiles to CSS
 * [Theming](https://github.com/cssinjs/theming) as the theming solution for React
 * [Typescript](https://www.typescriptlang.org/) for a high level of structure and strong typing
 * [Webpack](https://webpack.js.org/) to bundle assets and run a live reloading dev server
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,8 +3,7 @@ import * as React from "react";
 import { ITheme, primary } from "../main";
 import { withTheme } from "@callstack/react-theme-provider";
 import { Parallax, ParallaxLayer } from "react-spring";
-// @ts-ignore
-import * as ReadingProgress from 'react-reading-progress'
+import MediaQuery from 'react-responsive';
 
 
 
@@ -13,6 +12,10 @@ var samAndShaun1 = require("../../assets/photos/SamAndShaun_2_Portrait.jpg");
 var samAndShaun2 = require("../../assets/photos/SamAndShaun_1_Landscape.jpg");
 var samAndShaun3 = require("../../assets/photos/SamAndShaun_3_Landscape.jpg");
 
+
+class Home1 extends MediaQuery {
+
+}
 
 const url = (name: any, wrap = false) =>
     `${
@@ -39,7 +42,7 @@ export class Home extends React.Component<IHomeProps, any> {
     }
 
     handleScroll(event: any): void {
-        var h: any =  this.parallaxContainer.container,
+        var h: any = this.parallaxContainer.container,
             b: any = document.body,
             st: any = 'scrollTop',
             sh: any = 'scrollHeight';
@@ -50,206 +53,208 @@ export class Home extends React.Component<IHomeProps, any> {
 
     render(): JSX.Element {
         return (
-            <Parallax
-                style={this.props.theme.main}
-                ref={(ref: any) => {
-                    this.parallaxContainer = ref;
-                    this.props.setParallaxContainer(ref);
-                }}
-                pages={3}>
-                <ParallaxLayer
-                    offset={1}
-                    speed={1}
-                />
-                <ParallaxLayer
-                    offset={2}
-                    speed={1}
-                />
-
-                <ParallaxLayer
-                    offset={0}
-                    speed={0}
-                    factor={3}
-                />
-
-                <ParallaxLayer
-                    offset={1.3}
-                    speed={-0.3}
-                    style={{ pointerEvents: 'none' }}>
-                    <img
-                        src={url('satellite4')}
-                        style={{ width: '15%', marginLeft: '70%' }}
-                    />
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '20%', marginLeft: '55%' }}
-                    />
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '10%', marginLeft: '15%' }}
-                    />
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '20%', marginLeft: '70%' }}
-                    />
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '20%', marginLeft: '40%' }}
-                    />
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '10%', marginLeft: '10%' }}
-                    />
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '20%', marginLeft: '75%' }}
-                    />
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '20%', marginLeft: '60%' }}
-                    />
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '25%', marginLeft: '30%' }}
-                    />
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '10%', marginLeft: '80%' }}
-                    />
-                </ParallaxLayer>
-
-                <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '20%', marginLeft: '5%' }}
-                    />
-                    <img
-                        src={url('cloud')}
-                        style={{ display: 'block', width: '15%', marginLeft: '75%' }}
-                    />
-                </ParallaxLayer>
-
-                <ParallaxLayer
-                    offset={2.5}
-                    speed={-0.4}
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        pointerEvents: 'none',
-                    }}>
-                    <img src={url('earth')} style={{ width: '60%' }} />
-                </ParallaxLayer>
-
-                <ParallaxLayer
-                    offset={2}
-                    speed={-0.3}
-                    style={{
-                        backgroundSize: '80%',
-                        backgroundPosition: 'center',
-                        backgroundImage: url('clients', true),
+            <MediaQuery query="(min-device-width: 300px)">
+                <Parallax
+                    style={this.props.theme.main}
+                    ref={(ref: any) => {
+                        this.parallaxContainer = ref;
+                        this.props.setParallaxContainer(ref);
                     }}
-                />
+                    pages={3}>
+                    <ParallaxLayer
+                        offset={1}
+                        speed={1}
+                    />
+                    <ParallaxLayer
+                        offset={2}
+                        speed={1}
+                    />
 
-                <ParallaxLayer
-                    offset={0}
-                    speed={0}
-                    style={{
-                        display: 'flex',
-                        paddingTop: '75px',
-                        flexFlow: 'column wrap'
-                    }}>
-                    <img
-                        width={400}
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0}
+                        factor={3}
+                    />
+
+                    <ParallaxLayer
+                        offset={1.3}
+                        speed={-0.3}
+                        style={{ pointerEvents: 'none' }}>
+                        <img
+                            src={url('satellite4')}
+                            style={{ width: '15%', marginLeft: '70%' }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '20%', marginLeft: '55%' }}
+                        />
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '10%', marginLeft: '15%' }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '20%', marginLeft: '70%' }}
+                        />
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '20%', marginLeft: '40%' }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '10%', marginLeft: '10%' }}
+                        />
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '20%', marginLeft: '75%' }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '20%', marginLeft: '60%' }}
+                        />
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '25%', marginLeft: '30%' }}
+                        />
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '10%', marginLeft: '80%' }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '20%', marginLeft: '5%' }}
+                        />
+                        <img
+                            src={url('cloud')}
+                            style={{ display: 'block', width: '15%', marginLeft: '75%' }}
+                        />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                        offset={2.5}
+                        speed={-0.4}
                         style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            pointerEvents: 'none',
+                        }}>
+                        <img src={url('earth')} style={{ width: '60%' }} />
+                    </ParallaxLayer>
+
+                    <ParallaxLayer
+                        offset={2}
+                        speed={-0.3}
+                        style={{
+                            backgroundSize: '80%',
+                            backgroundPosition: 'center',
+                            backgroundImage: url('clients', true),
+                        }}
+                    />
+
+                    <ParallaxLayer
+                        offset={0}
+                        speed={0}
+                        style={{
+                            display: 'flex',
+                            paddingTop: '75px',
+                            flexFlow: 'column wrap'
+                        }}>
+                        <img
+                            width={400}
+                            style={{
+                                alignSelf: 'center',
+                            }}
+                            src={samAndShaun} />
+                        <p style={{
+                            fontFamily: "Imperator",
+                            color: primary,
                             alignSelf: 'center',
-                        }}
-                        src={samAndShaun} />
-                    <p style={{
-                        fontFamily: "Imperator",
-                        color: primary,
-                        alignSelf: 'center',
-                        fontSize: "25px",
-                        textAlign: "center"
-                    }}>Friday, October 29th 2019<br />St. Margaret Mary Alacoque Church<br />Harrisburg, PA</p>
-                </ParallaxLayer>
+                            fontSize: "25px",
+                            textAlign: "center"
+                        }}>Friday, October 29th 2019<br />St. Margaret Mary Alacoque Church<br />Harrisburg, PA</p>
+                    </ParallaxLayer>
 
-                <ParallaxLayer
-                    offset={0.25}
-                    speed={0.5}
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                    }}>
-                    <img
+                    <ParallaxLayer
+                        offset={0.25}
+                        speed={0.5}
                         style={{
-                            width: '500px',
-                            marginRight: '40%',
-                            transform: 'rotate(-10deg)',
-                            backgroundColor: 'white',
-                            padding: '10px',
-                            display: 'block',
-                            boxShadow: '9px 9px 5px -3px rgba(0,0,0,0.22)'
-                        }}
-                        src={samAndShaun2} />
-                </ParallaxLayer>
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                        }}>
+                        <img
+                            style={{
+                                width: '500px',
+                                marginRight: '40%',
+                                transform: 'rotate(-10deg)',
+                                backgroundColor: 'white',
+                                padding: '10px',
+                                display: 'block',
+                                boxShadow: '9px 9px 5px -3px rgba(0,0,0,0.22)'
+                            }}
+                            src={samAndShaun2} />
+                    </ParallaxLayer>
 
-                <ParallaxLayer
-                    offset={0.25}
-                    speed={0.3}
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <img
+                    <ParallaxLayer
+                        offset={0.25}
+                        speed={0.3}
                         style={{
-                            width: '500px',
-                            marginLeft: '40%',
-                            transform: 'rotate(10deg)',
-                            backgroundColor: 'white',
-                            padding: '10px',
-                            display: 'block',
-                            boxShadow: '9px 9px 5px -3px rgba(0,0,0,0.22)'
-                        }}
-                        src={samAndShaun3} />
-                </ParallaxLayer>
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <img
+                            style={{
+                                width: '500px',
+                                marginLeft: '40%',
+                                transform: 'rotate(10deg)',
+                                backgroundColor: 'white',
+                                padding: '10px',
+                                display: 'block',
+                                boxShadow: '9px 9px 5px -3px rgba(0,0,0,0.22)'
+                            }}
+                            src={samAndShaun3} />
+                    </ParallaxLayer>
 
-                <ParallaxLayer
-                    offset={1}
-                    speed={0.1}
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <img src={url('bash')} style={{ width: '40%' }} />
-                </ParallaxLayer>
+                    <ParallaxLayer
+                        offset={1}
+                        speed={0.1}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <img src={url('bash')} style={{ width: '40%' }} />
+                    </ParallaxLayer>
 
-                <ParallaxLayer
-                    offset={2}
-                    speed={0.1}
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                    }}>
-                    <img src={url('clients-main')} style={{ width: '40%' }} />
-                </ParallaxLayer>
-            </Parallax>
+                    <ParallaxLayer
+                        offset={2}
+                        speed={0.1}
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}>
+                        <img src={url('clients-main')} style={{ width: '40%' }} />
+                    </ParallaxLayer>
+                </Parallax>
+            </MediaQuery>
         )
     }
 }
