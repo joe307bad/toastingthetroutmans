ab37c20d249cd23a770c468f8ae7e7166a583252
Joe Badaczewski <badaczewskij@live.com>
2019-01-10 17:14:56 -0500

    adding flower pictures and groomsmen/bridesmaids pictures

--- src/assets/photos/Photos.tsx
+++ src/assets/photos/Photos.tsx
@@ -1,6 +1,13 @@
 import * as Church_1 from './church/Church_1.jpg';
 import * as Church_2 from './church/Church_2.jpg';
 import * as Flowers from './flowers.png';
+import * as FlowerPic1 from './flowers/flowers1.png';
+import * as FlowerPic2 from './flowers/flowers2.png';
+import * as FlowerPic3 from './flowers/flowers3.png';
+import * as FlowerPic4 from './flowers/flowers4.png';
+import * as FlowerPic5 from './flowers/flowers5.png';
+import * as FlowerPic6 from './flowers/flowers6.png';
+import * as FlowerPic7 from './flowers/flowers7.png';
 import * as Flowers1 from './flowers1.png';
 import * as CrateAndBarrel from './registries/crate-and-barrel.png';
 import * as Target from './registries/target.png';
@@ -13,7 +20,7 @@ import * as SamAndShaun_4_Landscape from './SamAndShaun_4_Landscape.jpg';
 import * as CurlyUnderline from './underline.png';
 
 /**
- * List of photos used within the engagement website\n
+ * List of photos used within the engagement website
  */
 export const Photos: { [key: string]: string } = {
     samAndShaun: SamAndShaun,
@@ -23,6 +30,13 @@ export const Photos: { [key: string]: string } = {
     SamAndShaun_4_Landscape: SamAndShaun_4_Landscape,
     Flowers: Flowers,
     Flowers1: Flowers1,
+    FlowerPic1: FlowerPic1,
+    FlowerPic2: FlowerPic2,
+    FlowerPic3: FlowerPic3,
+    FlowerPic4: FlowerPic4,
+    FlowerPic5: FlowerPic5,
+    FlowerPic6: FlowerPic6,
+    FlowerPic7: FlowerPic7,
     CurlyUnderline: CurlyUnderline,
     Church_1: Church_1,
     Church_2: Church_2,
